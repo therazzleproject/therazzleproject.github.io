8a54fc2b9eba849dfdcace8c62d6558548acdb3a
parker <karasolomon2010@gmail.com>
2021-04-16 13:01:49 -0700

    Update therazzleproject.js

--- therazzleproject.js
+++ therazzleproject.js
@@ -1,1 +1,15 @@
-
+function talk (string) {
+  alert(string);
+});
+function add (int1, int2) {
+  return int1 + int2;
+});
+function subtract (int1, int2) {
+  return int1 - int2;
+});
+function multiply (int1, int2) {
+return int1 * int2;
+});
+function divide (int1, int2) {
+  return int1 / int2;
+});
